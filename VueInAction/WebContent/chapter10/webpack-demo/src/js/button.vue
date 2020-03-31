b7247958f7acaca82a74612f641cc7a506488907
xuhongbao <xuhongbao1@163.com>
2020-03-31 21:32:43 +0800

    修改第十章webpack开发和生产环境打包配置文件

---
+++ VueInAction/WebContent/chapter10/webpack-demo/src/js/button.vue
@@ -0,0 +1,41 @@
+<template>
+	<button @click="handleClick" :style="styles">
+		<slot></slot>
+	</button>
+</template>
+<script>
+	export default {
+		props: {
+			color: {
+				type: String,
+				default: '#00cc66'
+			}
+		},
+		computed: {
+			styles() {
+				return {
+					background: this.color
+				}
+			}
+		},
+		methods: {
+			handleClick (e) {
+				this.$emit('click', e);
+			}
+		}
+	}
+</script>
+<style scoped>
+	/* 来自button.vue文件<styel>标签样式：使用scoped属性，可以为生成HTML标签和css样式生成data-v-xxx属性，使样式只对当前组件有效 */
+	button {
+		border: 0;
+		outline: none;
+		color: #fff;
+		padding: 4px 8px;
+	}
+	button:active {
+		position: relative;
+		top: 1px;
+		left: 1px;
+	}
+</style>
---
+++ VueInAction/WebContent/chapter10/webpack-demo/src/js/title.vue
@@ -0,0 +1,21 @@
+<template>
+	<h1>
+		<a :href="'#' + title">{{ title }}</a>
+	</h1>
+</template>
+<script>
+	export default {
+		props: {
+			title: {
+				type: String
+			}
+		}
+	}
+</script>
+<style scoped>
+	/* 来自title.vue文件<styel>标签样式：使用scoped属性，可以为生成HTML标签和css样式生成data-v-xxx属性，使样式只对当前组件有效 */
+	h1 a {
+		color: #3399ff;
+		font-size: 24px;
+	}
+</style>
